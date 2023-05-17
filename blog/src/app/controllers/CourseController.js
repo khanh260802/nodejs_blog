e2d2035b132e4e575f72c3810eaf278e449cf505
khanh260802 <casiofx580@gmail.com>
2023-05-18 02:44:04 +0700

    checkbox select all

--- blog/src/app/controllers/CourseController.js
+++ blog/src/app/controllers/CourseController.js
@@ -1,76 +1,102 @@
-const Course = require('../models/Course');
-const { mongooesToObject } = require('../../util/mongoose');
-const { json } = require('express');
+const Course = require("../models/Course");
+const { mongooesToObject } = require("../../util/mongoose");
+const { json } = require("express");
 class CourseController {
     // [GET] /course/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
             .then((course) => {
-                res.render('course/show', { course: mongooesToObject(course) });
+                res.render("course/show", { course: mongooesToObject(course) });
             })
             .catch(next);
     }
 
     // [GET] /courses/create
     create(req, res, next) {
-        res.render('course/create');
+        res.render("course/create");
     }
 
     // [POST] /courses/saved
     save(req, res, next) {
         const data = req.body;
         data.image = `https://img.youtube.com/vi/${data.videoID}/maxresdefault.jpg`;
-        
+
         const course = new Course(data);
-        course.save()
-            .then( res.redirect('/me/stored-courses') )
-            .catch(next);
+        course.save().then(res.redirect("/me/stored-courses")).catch(next);
     }
 
     // [GET] /courses/edit
     edit(req, res, next) {
-        Course.findOne({_id : req.params.id }) 
-            .then(course => {
-                res.render('course/edit', {
-                    course : mongooesToObject(course)
-                }); 
+        Course.findOne({ _id: req.params.id })
+            .then((course) => {
+                res.render("course/edit", {
+                    course: mongooesToObject(course),
+                });
             })
-            .catch(next)
+            .catch(next);
     }
-    
-     // [PUT] /courses/:id
+
+    // [PUT] /courses/:id
     update(req, res, next) {
-        const data = req.body; 
-        Course.updateOne({_id : req.params.id}, data)
-            .then(() => res.redirect('/') )
-            .catch(next)
+        const data = req.body;
+        data.image = `https://img.youtube.com/vi/${data.videoID}/maxresdefault.jpg`;
+        Course.updateOne({ _id: req.params.id }, data)
+            .then(() => res.redirect("/"))
+            .catch(next);
     }
 
-    // [PATCH] /courses/:id 
+    // [PATCH] /courses/:id
     delete(req, res, next) {
-        Course.delete( {_id: req.params.id} )
-            .then(() => res.redirect('back')) 
-            .catch(next) 
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
     }
-    
-    // [PATCH] /courses/:id/restore 
-    restore(req, res, next) { 
 
-        Course.restore({_id: req.params.id})
-            .then(() => res.redirect('back'))
-            .catch(next)
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
 
         //
     }
 
-
     // [DELETE] /courses/:id/destroy
-    destroy(req, res, next) { 
-        Course.deleteOne({_id: req.params.id}) 
-            .then(() => res.redirect('back')) 
-            .catch(next)
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
     }
-    
 
+    // [POST] /courses/actions
+    actions(req, res, next) {
+        switch (req.body["select-action"]) {
+            case "delete":
+                Course.delete({ _id: { $in: req.body["checkbox-items"] } })
+                    .then(res.redirect("back"))
+                    .catch("next");
+                break;
+            default:
+                res.send("The action's not valid");
+        }
+    }
+
+    // [POST] /courses/actions-from-trash
+    actionsTrash(req, res, next) {
+        switch (req.body["select-action"]) {
+            case "restore":
+                Course.restore({ _id: { $in: req.body["checkbox-items"] } })
+                    .then(res.redirect("back"))
+                    .catch("next");
+                break;
+            case "destroy":
+                Course.deleteMany({ _id: { $in: req.body["checkbox-items"] } })
+                    .then(res.redirect("back"))
+                    .catch("next");
+                break;
+            default:
+                res.send("The action's not valid");
+        }
+    }
 }
 module.exports = new CourseController();
--- blog/src/routes/courses.js
+++ blog/src/routes/courses.js
@@ -5,10 +5,12 @@ const CourseController = require('../app/controllers/CourseController');
 
 
 
+router.post('/actions-from-trash', CourseController.actionsTrash);  
 
-router.get('/:id/edit', CourseController.edit); 
+router.post('/actions', CourseController.actions); 
 router.post('/saved', CourseController.save); 
 router.get('/create', CourseController.create); 
+router.get('/:id/edit', CourseController.edit); 
 router.delete('/:id/destroy', CourseController.destroy); 
 router.patch('/:id/restore', CourseController.restore); 
 router.patch('/:id', CourseController.delete);
